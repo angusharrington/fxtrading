d2c14867a874570ed9005b4ec21137a2d99a878f
Your NameAngus <ah2077@cam.ac.uk>
2021-01-04 12:13:19 +0000

    get data function added

--- fxtrading.py
+++ fxtrading.py
@@ -30,38 +30,3 @@ begin_unix=time_converter("2018-01-01 00:00:00")
 end_unix=time_converter("2018-01-07 00:00:00")
 
 print(begin_unix, end_unix)
-#%%
-i=begin_unix+step
-dataset=pd.DataFrame()
-params={"from": str(i-step),
-        "to": str(i),
-        "granularity":granularity,
-        "price":'A' } # 'A' stands for ask price; 
-                      # if you want to get Bid use 'B' instead or 'AB' for both.
-while i<=end_unix:
-    params['from']=str(i-step)
-    params['to']=str(i)
-    r=instruments.InstrumentsCandles(instrument="EUR_USD",params=params)
-    data = client.request(r)
-    results= [{"time":x['time'],"open":float(x['ask']['o']),"high":float(x['ask']['h']),
-              "low":float(x['ask']['l']),"close":float(x['ask']['c']),"volume":float(x['volume'])} for x in data['candles']]
-    df = pd.DataFrame(results)
-    if dataset.empty: dataset=df.copy()
-    else: dataset=dataset.append(df, ignore_index=True)
-    if(i+step)>=end_unix:
-        params['from']=str(i)
-        params['to']=str(end_unix)
-        r=instruments.InstrumentsCandles(instrument="EUR_USD",params=params)
-        data = client.request(r)
-        results= [{"time":x['time'],"open":float(x['ask']['o']),"high":float(x['ask']['h']),
-                  "low":float(x['ask']['l']),"close":float(x['ask']['c'])} for x in data['candles']]
-        df = pd.DataFrame(results)
-        i+=step
-        dataset=dataset.append(df, ignore_index=True)
-    if len(dataset)>2000000:
-        dataset.to_csv("EURUSD"+"_"+granularity+"_"+dataset['time'][0].split('T')[0]+"_"+dataset['time'][len(dataset)-1].split('T')[0]+'.csv',index=False)
-        dataset=pd.DataFrame()
-    i+=step
-dataset.to_csv("EURUSD"+"_"+granularity+"_"+dataset['time'][0].split('T')[0]+"_"+dataset['time'][len(dataset)-1].split('T')[0]+'.csv',index=False)
-
-# %%
